2ee8b5e68cec71611e3d68b99bc0415419c5c0c7
Darrik Mazey <darrik@darmasoft.com>
2010-02-11 10:33:36 -0500

    support for selected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,35 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-	def accounts_options_for_select
+	def accounts_options_for_select(selected_id = nil)
 		option_tags = Array.new
 		@current_user.accounts.root.each do |a|
-			option_tags << "<option value=\"#{a.id}\">#{a.name}</option>"
+			option_tags << "<option value=\"#{a.id}\" #{account_selected(a.id, selected_id)}>#{a.name}</option>"
 			if a.children.size > 0
 				option_tags << "<optgroup>"
-				option_tags << options_from_account(a)
+				option_tags << options_from_account(a, selected_id)
 				option_tags << "</optgroup>"
 			end
 		end
 		option_tags.flatten.join
 	end
 
-	def options_from_account(account)
+	def options_from_account(account, selected_id = nil)
 		option_tags = Array.new
 		account.children.each do |c|
-			option_tags << "<option value=\"#{c.id}\">#{c.name}</option>"
+			option_tags << "<option value=\"#{c.id}\" #{account_selected(c.id, selected_id)}>#{c.name}</option>"
 			if c.children.size > 0
 				option_tags << options_from_account(c)
 			end
 		end
 		option_tags
+	end
+
+	def account_selected(aid, sid)
+		if aid == sid
+			'selected'
+		else
+			''
+		end
 	end
 end
