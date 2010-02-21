5b8277cfd25f0723d58edac4bc79665576c388cb
Darrik Mazey <darrik@darmasoft.com>
2010-02-21 01:23:55 -0500

    remove 'first' class

--- lib/finance_field_builder.rb
+++ lib/finance_field_builder.rb
@@ -1,6 +1,6 @@
 class FinanceFieldBuilder
   def field(name, value)
-    "<div class=\"field first #{name.downcase}\">
+    "<div class=\"field #{name.downcase}\">
       <div class=\"field_name\">#{name.empty? ? "&nbsp;" : name + ":"}</div>
       <div class=\"field_value\">#{value}</div>
     </div>"
