3480b5fe1d07f47184743866f03d655073ddc2b8
Darrik Mazey <darrik@darmasoft.com>
2010-02-23 15:01:14 -0500

    minor view revisions for work_items; comment flow improvement

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -55,7 +55,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         flash[:notice] = 'Comment was successfully created.'
-        format.html { redirect_to(@comment.work_item) }
+        format.html { redirect_to work_items_url }
         format.xml  { render :xml => @comment, :status => :created, :location => @comment }
       else
         format.html { render :action => "new" }
