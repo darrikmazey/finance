97f56ae7fc3fa3fb6cd441a3752ee713bc1daaf7
Matthew Tidd (DMT Key <matt@dmtprogramming.com>
2010-02-26 14:18:04 -0500

    added a template field to the user_option table, added a user_option form in the user/edit page.  now users's can select their template and it defaults to dark

--- app/models/user_option.rb
+++ app/models/user_option.rb
@@ -1,7 +1,26 @@
 class UserOption < TablelessModel
   column :account_group_id, :id
   column :user_id, :id
+  column :template, :string
 
   belongs_to :user
   belongs_to :account_group
+
+  # make sure the template exists and is valid
+  def template
+    tem = self.read_attribute('template') 
+    tem = UserOption.templates.first unless tem
+    tem = UserOption.templates.first unless UserOption.templates.include?(tem)
+    self.write_attribute('template', tem)
+    return tem
+  end
+
+  # get the list of templates from the template folder
+  def self.templates
+    if !@user_option_templates
+      Dir.chdir("#{RAILS_ROOT}/public/stylesheets/templates/")
+      @user_option_templates = Dir['*.css'].collect { |c| c.gsub('.css', '') }
+    end
+    return @user_option_templates
+  end
 end
