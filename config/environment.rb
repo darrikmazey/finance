27eb2f9acc51ad5e15764ab6a7874a9b9aff2b76
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2010-03-09 01:27:40 -0500

    froze rails and version 0.8.54 of the prawn gem into the vendor directory

--- config/environment.rb
+++ config/environment.rb
@@ -14,6 +14,10 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
+  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir|
+    File.directory?(lib = "#{dir}/lib") ? lib : dir
+  end
+
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
