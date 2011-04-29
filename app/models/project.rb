a5fa8f3038e69d46e45685faa1fdea323767759e
Darrik Mazey <darrik@darmasoft.com>
2011-04-29 17:16:39 -0400

    some metric totals

--- app/models/project.rb
+++ app/models/project.rb
@@ -17,4 +17,21 @@ class Project < ActiveRecord::Base
   def account_group
     client.account.account_group rescue nil
   end
+
+	def ytd
+		self.invoices.select {|i| !i.paid_at.nil? && i.paid_at >= Date.new(Date.today.year, 1, 1) }.sum(&:total)
+	end
+
+	def last_12_months
+		self.invoices.select { |i| !i.paid_at.nil? && i.paid_at >= 12.months.ago }.sum(&:total)
+	end
+
+	def last_calendar_year
+		self.invoices.select { |i| !i.paid_at.nil? && i.paid_at >= Date.new(Date.today.year - 1, 1, 1) && i.paid_at < Date.new(Date.today.year, 1, 1) }.sum(&:total)
+	end
+
+	def ytd_last_year
+		self.invoices.select { |i| !i.paid_at.nil? && i.paid_at >= Date.new(Date.today.year - 1, 1, 1) && i.paid_at < Date.new(Date.today.year - 1, Date.today.month, Date.today.day) }.sum(&:total)
+	end
+
 end
